174fa5ef212344aacbcc484c84f743f8731041cd
aymenamin2026 <aye122men122@gmail.com>
2026-06-15 06:21:27 +0300

    fix  price 5

--- src/app/shop/[id]/ProductClient.tsx
+++ src/app/shop/[id]/ProductClient.tsx
@@ -452,10 +452,10 @@ export default function ProductClient({
                   </span>
                 </span>
               </div>
-              <div className="flex items-center gap-3 text-sm text-muted-foreground">
+              {/* <div className="flex items-center gap-3 text-sm text-muted-foreground">
                 <RefreshCw className="w-5 h-5 text-foreground" />
                 <span>{t('free_returns', lang)}</span>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
