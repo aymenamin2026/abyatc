a172d645ebc22c972523f969fa36f2e082648e92
aymenamin2026 <aye122men122@gmail.com>
2026-07-13 10:49:05 +0300

    fix all web 1

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest) {
   try {
     revalidatePath('/', 'layout');
-    revalidateTag('settings');
+    revalidateTag('settings', { expire: 0 });
     return NextResponse.json({ revalidated: true, now: Date.now() });
   } catch (err) {
     return NextResponse.json({ message: 'Error revalidating' }, { status: 500 });
